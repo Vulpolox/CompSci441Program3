9bdd6f25132e6827425a0155fcf612e15ecf3eb6
Vulpolox <jack.w.a.keys@gmail.com>
2025-04-02 15:10:03 -0500

    Added some error handling and fixed some bugs

--- src/bindings.rkt
+++ src/bindings.rkt
@@ -1,42 +1,44 @@
 #lang racket
 
 (require data/either)
-(provide keyword-hash)
+(provide (all-defined-out))
 
 (define keyword-hash
   (make-immutable-hash
    '(
      ('num . (λ (n) (num n)))
-     ('id . (λ (i) (id i)))
+     ('id . (λ (state i) (id state i)))
      ('add . (λ (n1 n2) (add n1 n2)))
      ('sub . (λ (n1 n2) (sub n1 n2)))
      ('div . (λ (n1 n2) (div n1 n2)))
      ('mult . (λ (n1 n2) (mult n1 n2)))
-     ('assign . (λ (id expr) (assign id expr)))
-     ('define . (λ (id name [val "undefined"]) (def id name val)))
-     ('remove . (λ (id) (rem id)))
+     ('assign . (λ (state i expr) (assign state i expr)))
+     ('define . (λ (state id [val "undefined"]) (def state id val)))
+     ('remove . (λ (state id) (rem id)))
     )
   )
 )
 
 ;; simple boolean returning functions
 (define (list-contains? lst e) (ormap (λ (element) (equal? e element)) lst))
-(define (is-id? symbol) (list-contains? ('num 'id) symbol))
-(define (is-op? symbol) (list-contains? ('add 'sub 'mult 'div) symbol))
-(define (is-state-modifier? symbol) (list-contains? ('assign 'define 'remove)))
+(define (is-id? symbol) (equal? symbol 'id))
+(define (is-num? symbol) (equal? symbol 'num))
+(define (is-op? symbol) (list-contains? '(add sub mult div) symbol))
+(define (is-state-modifier? symbol) (list-contains? (list 'assign 'define 'remove) symbol))
 (define (is-assign? symbol) (equal? symbol 'assign))
 (define (is-define? symbol) (equal? symbol 'define))
 (define (is-remove? symbol) (equal? symbol 'remove))
-(define (is-invalid? symbol) (not (hash-has-key? keyword-hash symbol)))
+(define (is-invalid? symbol) 
+        (not (or (is-id? symbol) (is-num? symbol) (is-op? symbol) (is-state-modifier? symbol)))
+)
 
 
 ;; function for returning the arity of a keyword function
 (define (get-arity symbol)
     (cond
-        [(is-id? symbol) 1]
-        [(is-op? symbol) 2]
-        [(is-assign? symbol) 2]
-        [(is-remove? symbol) 1]
+        [(or (is-remove? symbol) (is-num? symbol) (is-id? symbol)) 1]
+        [(or (is-op? symbol) (is-assign? symbol)) 2]
+        [else -1]
     )
 )
 
@@ -85,4 +87,18 @@
         [(= n2 0) (format "ERROR: Divide By Zero: (div ~a ~a)" n1 n2)]
         [else (success (/ n1 n2))]
     )
+)
+
+
+(define (id state i)
+    (if
+        (hash-has-key? state i)
+        (success (hash-ref state i))
+        (failure (format "ERROR: Unbound Identifier -> ~a" i))
+    )
+)
+
+
+(define (assign state i expr)
+    "todo"
 )
--- src/expression-evaluator.rkt
+++ src/expression-evaluator.rkt
@@ -1,8 +1,69 @@
 #lang racket
 
 (require data/either)
+(require "./utils.rkt" "./bindings.rkt")
 (provide evaluate)
 
-(define identifier-hash (make-immutable-hash))
+;; signature: either<((expr | result | exn-message) state)> 
+;; -> either<((expr | result | exn-message) state)>
+(define (evaluate expr-and-state)
+    (define expr (first (extract expr-and-state)))
+    (define state (second (extract expr-and-state)))
+    (define current-func (if (empty? expr) 'empty (first expr)))
+    (define current-arguments (if (empty? expr) '() (rest expr)))
+    (define correct-arity (get-arity current-func))
 
-(define (evaluate expr) (identity expr))
+    (printf "~nLENGTH CURRENT ARGUMENTS: ~a~n" (length current-arguments))
+
+    (cond
+        ;; if evaluate is called on an expression that
+        ;; has already errored out, return itself
+        [(failure? expr-and-state) (identity (expr-and-state))]
+
+        ;; if evaluate is called on an empty expression
+        [(equal? current-func 'empty)
+         (failure (list "ERROR: Tried to Evaluate Empty Expression" state))
+        ]
+
+        ;; if the function pulled from the expression is not in
+        ;; '(num id add sub mult div assign remove define)
+        [(is-invalid? current-func) 
+         (failure (list (format "ERROR: Not A Function -> ~a" current-func) state))
+        ]
+
+        ;; special check for if the arity is correct if 'define is pulled
+        ;; as it can have either 1 or 2 arguments
+        [(and (is-define? current-func)
+               (nor (= 1 (length current-arguments))
+                    (= 2 (length current-arguments))
+               )
+         )
+         (failure (list (format "ERROR: Arity Mismatch for \'define\': Got ~a Arguments, Expected 1 or 2"
+                                (length current-arguments)
+                        ) 
+                    state
+                  )
+         )
+        ]
+
+        ;; if the function is called with the incorrect amount of arguments
+        [(not (= correct-arity (length current-arguments)))
+         (failure (list (format "ERROR: Arity Mismatch for \'~a\': Got ~a Arguments, Expected ~a"
+                                current-func
+                                (length current-arguments)
+                                correct-arity
+                        )
+                    state
+                  )
+         ) 
+        ]
+
+        ;; if the function pulled is an operator
+        [(is-op? current-func)
+         (cond
+            [#t #f]
+         )
+        ]
+
+    )
+)
